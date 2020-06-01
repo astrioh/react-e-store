e1de1257af6ecf0ebfef9ba0fbf854be8c7d5a25
AstRioh <korova674@gmail.com>
2020-06-01 22:18:29 +0300

    Started routing setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.scss';
 import Homepage from './pages/Homepage/Homepage';
+import Shop from './pages/Shop/Shop';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={Shop} />
+      </Switch>
+      
     </div>
   );
 }
